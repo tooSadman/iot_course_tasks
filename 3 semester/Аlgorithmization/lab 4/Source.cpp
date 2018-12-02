52c2101c45bdbb5b6928763dd7a3a97b7ef70549
Nick <mykola.kb@gmail.com>
2018-12-02 22:50:28 +0200

    Added algo lab 4

---
+++ 3 semester/Аlgorithmization/lab 4/Source.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+
+using namespace std;
+
+
+
+void dfs (int v, vector<vector<int> > &graph, vector<bool> &used, vector<set<int> > &counter)
+{
+								used[v] = true;
+
+                // inserting in counter neighbours
+								counter[v].insert(graph[v].begin(), graph[v].end());
+								// inserting neighbours of neighbours
+								for (vector<int>::iterator j = graph[v].begin(); j != graph[v].end(); ++j)
+																counter[v].insert(graph[*j].begin(), graph[*j].end());
+
+
+								for (vector<int>::iterator i = graph[v].begin(); i != graph[v].end(); ++i)
+																if (!used[*i])
+																								dfs (*i, graph, used, counter);
+
+}
+
+vector<vector<char> > readMatrix()
+{
+								string s;
+								cin >> s;
+
+								vector<vector<char> > matrix;
+								matrix.assign(s.length(), vector<char>());
+
+								for (int i = 0; i < s.length(); i++)
+								{
+																matrix[0].push_back(s[i]);
+								}
+
+
+								for (int i = 1; i < s.length(); i++)
+								{
+																cin >> s;
+																for (int j = 0; j < s.length(); j++)
+																{
+																								matrix[i].push_back(s[j]);
+																}
+								}
+
+								return matrix;
+}
+
+int main()
+{
+								auto relations = readMatrix();
+
+								vector<vector<int> > graph(relations.size());
+								for (int i = 0; i < relations.size(); i++)
+								{
+																for (int j = 0; j < relations.size(); j++)
+																{
+																								if (relations[i][j] == 'Y')
+																								{
+																																graph[i].push_back(j);
+																								}
+																}
+								}
+
+								vector<bool> used(relations.size());
+
+								vector<set<int> > counter(relations.size());
+
+								dfs(0, graph, used, counter);
+
+								int max = 0;
+
+								for (auto friends : counter)
+								{
+																if (max < friends.size() - 1)
+																{
+																								max = friends.size() - 1;
+																}
+								}
+
+								cout << max << endl;
+								return 0;
+}
