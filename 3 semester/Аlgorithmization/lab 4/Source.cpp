157eab497b86de59d90e5353bf01e18914f6c198
Nick <mykola.kb@gmail.com>
2018-12-04 19:08:53 +0200

    Updated algo lab 4

--- 3 semester/Аlgorithmization/lab 4/Source.cpp
+++ 3 semester/Аlgorithmization/lab 4/Source.cpp
@@ -7,81 +7,84 @@ using namespace std;
 
 
 
-void dfs (int v, vector<vector<int> > &graph, vector<bool> &used, vector<set<int> > &counter)
+void dfs (int v, vector<vector<int>> &graph, vector<bool> &used, vector<set<int>> &counter)
 {
-								used[v] = true;
-
-                // inserting in counter neighbours
-								counter[v].insert(graph[v].begin(), graph[v].end());
-								// inserting neighbours of neighbours
-								for (vector<int>::iterator j = graph[v].begin(); j != graph[v].end(); ++j)
-																counter[v].insert(graph[*j].begin(), graph[*j].end());
+	used[v] = true;
 
 
-								for (vector<int>::iterator i = graph[v].begin(); i != graph[v].end(); ++i)
-																if (!used[*i])
-																								dfs (*i, graph, used, counter);
+	counter[v].insert(graph[v].begin(), graph[v].end());
+
+	for (vector<int>::iterator j = graph[v].begin(); j != graph[v].end(); ++j)
+		counter[v].insert(graph[*j].begin(), graph[*j].end());
+
+
+	for (vector<int>::iterator i = graph[v].begin(); i != graph[v].end(); ++i)
+		if (!used[*i])
+			dfs (*i, graph, used, counter);
 
 }
 
-vector<vector<char> > readMatrix()
+vector<vector<char>> readMatrix()
 {
-								string s;
-								cin >> s;
+	string s;
+	cin >> s;
 
-								vector<vector<char> > matrix;
-								matrix.assign(s.length(), vector<char>());
+	vector<vector<char>> matrix;
+	matrix.assign(s.length(), vector<char>());
 
-								for (int i = 0; i < s.length(); i++)
-								{
-																matrix[0].push_back(s[i]);
-								}
+	for (int i = 0; i < s.length(); i++)
+	{
+		matrix[0].push_back(s[i]);
+	}
 
 
-								for (int i = 1; i < s.length(); i++)
-								{
-																cin >> s;
-																for (int j = 0; j < s.length(); j++)
-																{
-																								matrix[i].push_back(s[j]);
-																}
-								}
+	for (int i = 1; i < s.length(); i++)
+	{
+		cin >> s;
+		for (int j = 0; j < s.length(); j++)
+		{
+			matrix[i].push_back(s[j]);
+		}
+	}
 
-								return matrix;
+	return matrix;
 }
 
 int main()
 {
-								auto relations = readMatrix();
+	auto relations = readMatrix();
 
-								vector<vector<int> > graph(relations.size());
-								for (int i = 0; i < relations.size(); i++)
-								{
-																for (int j = 0; j < relations.size(); j++)
-																{
-																								if (relations[i][j] == 'Y')
-																								{
-																																graph[i].push_back(j);
-																								}
-																}
-								}
+	vector<vector<int>> graph(relations.size());
+	for (int i = 0; i < relations.size(); i++)
+	{
+		for (int j = 0; j < relations.size(); j++)
+		{
+			if (relations[i][j] == 'Y')
+			{
+				graph[i].push_back(j);
+			}
+		}
+	}
 
-								vector<bool> used(relations.size());
+	vector<bool> used(relations.size());
 
-								vector<set<int> > counter(relations.size());
+	vector<set<int>> counter(relations.size());
 
-								dfs(0, graph, used, counter);
+	dfs(0, graph, used, counter);
 
-								int max = 0;
+	int max = 0;
 
-								for (auto friends : counter)
-								{
-																if (max < friends.size() - 1)
-																{
-																								max = friends.size() - 1;
-																}
-								}
+	for (auto student : counter)
+	{
+		if (max < student.size() - 1)
+		{
+			max = student.size() - 1;
+		}
+	}
 
-								cout << max << endl;
-								return 0;
+	cout << max << endl;
+
+	system("pause");
+
+	return 0;
 }
