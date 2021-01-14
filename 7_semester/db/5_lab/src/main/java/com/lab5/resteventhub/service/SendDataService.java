cfc68adfa60719ce2b28e522c24a808afd575c8a
tooSadman <mykola.kb@gmail.com>
2021-01-14 02:45:38 +0200

    Clean up db lab 5

--- 7_semester/db/5_lab/src/main/java/com/lab5/resteventhub/service/SendDataService.java
+++
@@ -1,8 +0,0 @@
-package com.lab5.resteventhub.service;
-
-import com.microsoft.azure.eventhubs.EventHubException;
-import java.io.IOException;
-
-public interface SendDataService {
-    void sendAndLog(String url) throws IOException, EventHubException;
-}
